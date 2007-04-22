c4039a2396973a91254026f6b29dfa8d0fd0f9b1
Christian Theune <ct@gocept.com>
2007-04-22 10:42:48 +0000

    Updating version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='zope.app.principalannotation',
-      version='3.4dev',
+      version = '3.4.0a1',
       url='http://svn.zope.org/zope.app.principalannotation',
       license='ZPL 2.1',
       description='Zope principalannotation',
