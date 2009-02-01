8eeb0e9f6a855805872049d3c2402601ff9af81e
Christian Theune <ct@gocept.com>
2009-02-01 17:01:26 +0000

    pre-release cleanup, update metadata, insert release date

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 setup(name='zope.app.principalannotation',
       version = '3.5.0dev',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description='Annotations for Zope Principals',
       long_description=(
           read('README.txt')
@@ -42,7 +42,7 @@ setup(name='zope.app.principalannotation',
           'Operating System :: OS Independent',
           'Topic :: Internet :: WWW/HTTP',
           'Framework :: Zope3'],
-      url='http://cheeseshop.python.org/pypi/zope.app.principalannotation',
+      url='http://pypi.python.org/pypi/zope.app.principalannotation',
       license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
